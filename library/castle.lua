a61bf8ddbd3bdde4c8df176af4b0c4d627e2097a
trobukan <trobukan@gmail.com>
2025-07-18 23:53:14 -0300

    add getTouches method to castle

--- library/castle.lua
+++ library/castle.lua
@@ -27,3 +27,18 @@ end
 ---@param action function
 function castle.createTextBox(message, action)
 end
+
+---@class Touch
+---@field id number
+---@field x number
+---@field y number
+---@field deltaX number
+---@field deltaY number
+---@field initialX number
+---@field initialY number
+---@field pressed boolean
+---@field duration number
+
+---@return Touch[]
+function castle.getTouches()
+end
